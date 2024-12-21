e4b7012176dc6eb7a68baa7e68bd4bb3ade24245
tom96da <129831775+tom96da@users.noreply.github.com>
2024-12-21 14:37:12 +0900

    Initial Commit

---
+++ TrainUpTests/TrainUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrainUpTests.swift
+//  TrainUpTests
+//
+//  Created by tom96da on 2024/12/21.
+//
+
+import Testing
+@testable import TrainUp
+
+struct TrainUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
